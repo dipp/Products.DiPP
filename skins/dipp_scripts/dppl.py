98a61481b3549776540d844b84fc37f6a1ba5a65
DiPP Admin <dipp@hbz-nrw.de>
2008-04-24 21:28:47 +0000

    fixed typo in licence

--- skins/dipp_scripts/dppl.py
+++ skins/dipp_scripts/dppl.py
@@ -13,19 +13,19 @@ request  = container.REQUEST
 RESPONSE = request.RESPONSE
 
 if rights == 'DPPL':
-    details = {'en'  : 'Digital Peer Publishing Licence',
+    details = {'en'  : 'Digital Peer Publishing License',
                'de'  : 'Digital Peer Publishing Lizenz',
                'url' : {'de':'http://www.dipp.nrw.de/lizenzen/dppl/dppl/DPPL_v2_de_06-2004.html',
                         'en':'http://www.dipp.nrw.de/lizenzen/dppl/dppl/DPPL_v2_en_06-2004.html'}
               }
 elif rights == 'mDPPL':
-    details = {'en'  : 'modular Digital Peer Publishing Licence',
+    details = {'en'  : 'modular Digital Peer Publishing License',
                'de'  : 'modularen Digital Peer Publishing Lizenz',
                'url' : {'de':'http://www.dipp.nrw.de/lizenzen/dppl/mdppl/m-DPPL_v1_de_11-2004.html',
                         'en':'http://www.dipp.nrw.de/lizenzen/dppl/mdppl/m-DPPL_v1_de_11-2004.html'}
                }
 elif rights == 'fDPPL':
-    details = {'en'  : 'free Digital Peer Publishing Licence',
+    details = {'en'  : 'free Digital Peer Publishing License',
                'de'  : 'freien Digital Peer Publishing Lizenz',
                'url' : {'de':'http://www.dipp.nrw.de/lizenzen/dppl/fdppl/f-DPPL_v1_de_11-2004.html',
                         'en':'http://www.dipp.nrw.de/lizenzen/dppl/fdppl/f-DPPL_v1_de_11-2004.html'}
