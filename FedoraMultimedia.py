470dc1021f832fdd05480a1e929bff9efd72fa1c
Peter Reimer <reimer@hbz-nrw.de>
2009-04-30 09:58:34 +0000

    new method for getting the object. Sholf make fedoramultimedia_view obsolet. new field to categorize the object. Will be used for sepeate Listings of supplementary material.

--- FedoraMultimedia.py
+++ FedoraMultimedia.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from Products.Archetypes.public import *
+from Products.CMFCore.utils import getToolByName
+
 from config import PROJECTNAME
+from Products.CMFCore.permissions import View
+from Products.CMFCore.permissions import ModifyPortalContent
+from Products.CMFCore.utils import getToolByName
 from AccessControl import ClassSecurityInfo
 import Permissions
+from zLOG import LOG, ERROR, INFO
 from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
 
 class FedoraMultimedia(BrowserDefaultMixin, BaseContent):
@@ -15,23 +21,40 @@ class FedoraMultimedia(BrowserDefaultMixin, BaseContent):
         FileField('File',
                 required=0,
                 primary=1,
-                widget=FileWidget(label='File',description='Multimedia file')
+                widget=FileWidget(
+                    label='File',
+                    description='Multimedia file'
+                )
         ),
         StringField('PID',
                 required=0,
-                widget=StringWidget(label='PID',description='Persistent Identifier',size='15')
+                widget=StringWidget(
+                    label='Persistent Identifier (PID)',
+                    description='The PID of this digital object as given by the repository software. DO NOT EDIT.',
+                    size='15'
+                )
         ),
         StringField('DsID',
                 required=0,
-                widget=StringWidget(label='DsID',description='Datastream Identifier',size='15')
+                widget=StringWidget(
+                    label='Datastream Identifier (DsID)',
+                    description='The Datastream Identifier. DO NOT EDIT.',
+                    size='15')
         ),
-        BooleanField('supplement',
-                widget=BooleanWidget(label='Supplementary Material',description='Check this box, when the file should be listed with the supplementary material.'),
-                storage=AttributeStorage(),
-                searchable=1,
-                default=False,
-                required=1,
-                index="FieldIndex:brains"
+        StringField('Type',
+                required=0,
+                widget=SelectionWidget(
+                    label='Type',
+                    description='You can select, wether this file should be listed separatly in one of the categories below.'
+                ),
+                multiValued=0,
+                default = "",
+                index="FieldIndex:brains",
+                vocabulary = (
+                    ('','Do not list.'),
+                    ('alternative_format','Alternative format of the main text.'),
+                    ('supplementary_material','Supplementary Material')
+                )
         )
     ),
     marshall=PrimaryFieldMarshaller(),
@@ -41,6 +64,12 @@ class FedoraMultimedia(BrowserDefaultMixin, BaseContent):
     suppl_views = ('base_view', 'mp3_view')
     content_icon = "fedoramultimedia_icon.gif"
     
+    inlineMimetypes= ('application/msword',
+                      'application/vnd.ms-excel',
+                      'application/vnd.ms-powerpoint',
+                      'application/rtf',
+                      'application/pdf')
+
     actions = (
         { "id": "edit",
           "name": "Edit",
@@ -48,11 +77,11 @@ class FedoraMultimedia(BrowserDefaultMixin, BaseContent):
           "permissions": (Permissions.EDIT_CONTENTS_PERMISSION,),
           },
           
-        { "id": "view",
-          "name": "View",
-          "action": "string:${object_url}/fedoramultimedia_view",
-          "permissions": (Permissions.VIEW_CONTENTS_PERMISSION,),
-          },
+        #{ "id": "view",
+        #  "name": "View",
+        #  "action": "string:${object_url}/fedoramultimedia_view",
+        #  "permissions": (Permissions.VIEW_CONTENTS_PERMISSION,),
+        #  },
           
         { "id": "preview",
           "name": "Preview",
@@ -73,4 +102,36 @@ class FedoraMultimedia(BrowserDefaultMixin, BaseContent):
           "permissions": (Permissions.EDIT_CONTENTS_PERMISSION,),
           },
     )
+
+    security = ClassSecurityInfo()
+
+    security.declareProtected(View, 'index_html')
+    def index_html(self, REQUEST=None, RESPONSE=None):
+        """ fetch a file from the repository
+        """
+        
+        fedora = getToolByName(self, 'fedora')
+        PID = self.PID
+        DsID = self.DsID
+        data =  fedora.accessMultiMedia(PID,DsID,None)
+        stream = data['stream']
+        MIMEType =data['MIMEType']
+        RESPONSE.setHeader('Content-Type', MIMEType)
+        LOG('DiPP', INFO, MIMEType)
+        return stream
+
+    security.declareProtected(View, 'preview')
+    def preview(self, REQUEST=None, RESPONSE=None):
+        """Download the file
+        """
+        field = self.getPrimaryField()
+
+        if field.getContentType(self) in self.inlineMimetypes:
+            # return the PDF and Office file formats inline
+            return ATCTFileContent.index_html(self, REQUEST, RESPONSE)
+        # otherwise return the content as an attachment 
+        # Please note that text/* cannot be returned inline as
+        # this is a security risk (IE renders anything as HTML).
+        return field.download(self)
+
 registerType(FedoraMultimedia,PROJECTNAME)
