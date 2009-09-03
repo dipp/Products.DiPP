cccc7bb4dd56671462b054558cd21ba5e38273f5
Peter Reimer <reimer@hbz-nrw.de>
2009-09-03 06:34:44 +0000

    add efault to existing articles

--- skins/dipp_scripts/synchronize.py
+++ skins/dipp_scripts/synchronize.py
@@ -22,10 +22,27 @@ out = StringIO()
 
 articles   = container.portal_catalog(portal_type='FedoraArticle')
 for article in articles:
+    # only permanently stored articles are synchronized
+    # those with id/PID starting with 'temp' are skipped
     if article.id.split('_')[0] == 'temp':
         print >> out, "Skipping: " + article.getURL()
     else:
         obj = article.getObject()
-        obj.syncMetadata()        
-        print >> out, "Syncing: " + article.getURL()
+        # Journal section could be emty till now. It is required now
+        # and filled with a default 'no-section' Needed for the new
+        # issue_content_view template
+        if obj.journal_section == "":
+            print >> out, "FIX ME"
+            obj.setJournal_section('no-section')
+            obj.reindexObject()
+        print >> out, "Section:" + obj.journal_section
+        
+        # write the QDC Metadata stored in fedora to the available
+        # metadata fields of the Plone object
+        try:
+            obj.syncMetadata()
+            print >> out, "Syncing: " + article.getURL()
+        except:
+            print >> out, "Can't connect to Fedora repository, no syncing"
+
 return out.getvalue()
