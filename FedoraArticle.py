5e4ab94b02a44e7425ce45157a735f83407a1c7f
Peter Reimer <reimer@hbz-nrw.de>
2009-09-29 14:29:41 +0000

    new view template mwith mix of metadata and fulltext

--- FedoraArticle.py
+++ FedoraArticle.py
@@ -130,7 +130,7 @@ class FedoraArticle(BrowserDefaultMixin, OrderedBaseFolder):
     allowed_content_types = ('FedoraDocument','FedoraMultimedia','FedoraXML')
     immediate_view = 'base_view'
     default_view = 'base_view'
-    suppl_views = ('base_view', 'metadata_view', 'splash_screen')
+    suppl_views = ('base_view', 'metadata_view', 'mixed_view', 'splash_screen')
     filter_content_types= 1
     content_icon = 'fedoraarticle_icon.gif'
     actions = (
