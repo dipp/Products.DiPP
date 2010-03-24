531dca2dc44279ff03b1ed990ae4e4e08716b3ff
Peter Reimer <reimer@hbz-nrw.de>
2010-03-24 15:54:30 +0000

    all classifications in one dictionary

--- skins/fedora_content/classifications.py
+++ skins/fedora_content/classifications.py
@@ -7,21 +7,19 @@
 ##parameters=
 ##title=
 ##
-request = container.REQUEST
-RESPONSE =  request.RESPONSE
 
-Normdateien = [
-   {'id':'swd','title':'SWD (Schlagwortnormdatei)','url':'http://www.bsz-bw.de/cgi-bin/oswd-suche.pl'},
+classifactions = [
+    {'id':'swd', 'title':'SWD (Schlagwortnormdatei)', 'url':'http://www.bsz-bw.de/cgi-bin/oswd-suche.pl'},
+    {'id':'SWD', 'title':"Schlagwort Normdatei", 'url':'http://www.bsz-bw.de/cgi-bin/oswd-suche.pl'},
+    {'id':'LCSH ', 'title':"Library of Congress Subject Headings",'url':'http://authorities.loc.gov'},
+    {'id':'MeSH', 'title':"Medical Subject Headings", 'url':'http://www.nlm.nih.gov/mesh/meshhome.html'},
+    {'id':'DDC', 'title':"Dewey Decimal Classification", 'url':'http://www.oclc.org/dewey/versions/webdewey/default.htm'},
+    {'id':'DDC (de)', 'title':"DDC, Deutsche Version", 'url':'http://www.ddc-deutsch.de'},
+    {'id':'LCC ', 'title':"Library of Congress Classification", 'url':'http://www.loc.gov/catdir/cpso/lcco/lcco.html'},
+    {'id':'UDC', 'title':"Universal Decimal Classification", 'url':'http://www.udcc.org/outline/outline.htm'},
+    {'id':'RVK','title':"Regensburger Verbundklassifikation",'url':'http://www.bibliothek.uni-regensburg.de/rvko_neu/'},
+    {'id':'BK', 'title':"Basis-Klassifikation", 'url':'http://www.gbv.de/vgm/info/mitglieder/02Verbund/01Erschliessung/02Richtlinien/05Basisklassifikation/index'},
+    {'id':'PACS', 'title':"Physics and Astronomy Classification Scheme", 'url':'http://publish.aps.org/PACS/'},
+    {'id':'MSC','title':"2000 Mathematics Subject Classification", 'url':'http://www.ams.org/msc/'}
 ]
-#    'lcsh':'LCSH (Library of Congress Subject Headings)',
-#    'mesh':'MeSH (Medical Subject Headings)',
-#    'ddc' :'DDC (Dewey Decimal Classification)',
-#    'lcc' :'LCC (Library of Congress Classification)',
-#    'udc' :'UDC (Universal Decimal Classification)',
-#    'rvk' :'RVK (Regensburger Verbundklassifikation)',
-#    'bk'  :'BK (Basis-Klassifikation)',
-#    'pacs':'PACS (Physics and Astronomy Classification Scheme)',
-#    'msc' :'MSC (Mathematics Subject Classification)'
-#]
-
-return Normdateien
+return classifactions
