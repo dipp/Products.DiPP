b0c82d0851bc23ab59799b13963597af255eaa67
Peter Reimer <reimer@hbz-nrw.de>
2010-03-26 13:18:52 +0000

    classification need to be lowercase

--- skins/fedora_content/classifications.py
+++ skins/fedora_content/classifications.py
@@ -9,17 +9,15 @@
 ##
 
 classifactions = [
-    {'id':'swd', 'title':'SWD (Schlagwortnormdatei)', 'url':'http://www.bsz-bw.de/cgi-bin/oswd-suche.pl'},
-    {'id':'SWD', 'title':"Schlagwort Normdatei", 'url':'http://www.bsz-bw.de/cgi-bin/oswd-suche.pl'},
-    {'id':'LCSH ', 'title':"Library of Congress Subject Headings",'url':'http://authorities.loc.gov'},
-    {'id':'MeSH', 'title':"Medical Subject Headings", 'url':'http://www.nlm.nih.gov/mesh/meshhome.html'},
-    {'id':'DDC', 'title':"Dewey Decimal Classification", 'url':'http://www.oclc.org/dewey/versions/webdewey/default.htm'},
-    {'id':'DDC (de)', 'title':"DDC, Deutsche Version", 'url':'http://www.ddc-deutsch.de'},
-    {'id':'LCC ', 'title':"Library of Congress Classification", 'url':'http://www.loc.gov/catdir/cpso/lcco/lcco.html'},
-    {'id':'UDC', 'title':"Universal Decimal Classification", 'url':'http://www.udcc.org/outline/outline.htm'},
-    {'id':'RVK','title':"Regensburger Verbundklassifikation",'url':'http://www.bibliothek.uni-regensburg.de/rvko_neu/'},
-    {'id':'BK', 'title':"Basis-Klassifikation", 'url':'http://www.gbv.de/vgm/info/mitglieder/02Verbund/01Erschliessung/02Richtlinien/05Basisklassifikation/index'},
-    {'id':'PACS', 'title':"Physics and Astronomy Classification Scheme", 'url':'http://publish.aps.org/PACS/'},
-    {'id':'MSC','title':"2000 Mathematics Subject Classification", 'url':'http://www.ams.org/msc/'}
+    {'id':'swd', 'title':"Schlagwort Normdatei", 'url':'http://www.bsz-bw.de/cgi-bin/oswd-suche.pl'},
+    {'id':'lcsh', 'title':"Library of Congress Subject Headings",'url':'http://authorities.loc.gov'},
+    {'id':'mesh', 'title':"Medical Subject Headings", 'url':'http://www.nlm.nih.gov/mesh/meshhome.html'},
+    {'id':'ddc', 'title':"Dewey Decimal Classification", 'url':'http://www.oclc.org/dewey/versions/webdewey/default.htm'},
+    {'id':'lcc', 'title':"Library of Congress Classification", 'url':'http://www.loc.gov/catdir/cpso/lcco/lcco.html'},
+    {'id':'udc', 'title':"Universal Decimal Classification", 'url':'http://www.udcc.org/outline/outline.htm'},
+    {'id':'rvk','title':"Regensburger Verbundklassifikation",'url':'http://www.bibliothek.uni-regensburg.de/rvko_neu/'},
+    {'id':'bk', 'title':"Basis-Klassifikation", 'url':'http://www.gbv.de/vgm/info/mitglieder/02Verbund/01Erschliessung/02Richtlinien/05Basisklassifikation/index'},
+    {'id':'pacs', 'title':"Physics and Astronomy Classification Scheme", 'url':'http://publish.aps.org/PACS/'},
+    {'id':'msc','title':"2000 Mathematics Subject Classification", 'url':'http://www.ams.org/msc/'}
 ]
 return classifactions
