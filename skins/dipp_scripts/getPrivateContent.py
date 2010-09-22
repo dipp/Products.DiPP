b8b57f72338da3689ca6a2c3cb1a6def1f83e124
Peter Reimer <reimer@hbz-nrw.de>
2010-09-22 10:29:35 +0000

    Versionierung von privaten Fedorainhalten moeglich. Bisher war eine Authentifizierung notwendig, was dazu fuehrte, dass statt einer neuen Ver sion das login_formular im repository landete Jetzt uebernimmt ein script mit proxyrolle Manager die Aufgabe. Kann nur von der Maschine aufgerufen werden, auf der Fedora laeuft, um unberechigte Zugriffe zu vermeiden

--- skins/dipp_scripts/getPrivateContent.py
+++ skins/dipp_scripts/getPrivateContent.py
@@ -12,14 +12,37 @@
 from Products.CMFCore.utils import getToolByName
 request  = container.REQUEST
 RESPONSE = request.RESPONSE
-ALLOWED_IPS = ["193.30.112.98","193.30.112.23","10.1.1.57","10.1.1.67"]
-
-IP = request.HTTP_X_FORWARDED_FOR
 
 
+
+"""Allow fedora to fetch private content without authentication. This script runs with proxyrole Manager
+   but can only be called from the machine where fedora is installed.
 """
 
-if IP in ALLOWED_IPS:
+def get_ip(request):
+    """  Extract the client IP address from the HTTP request in proxy compatible way.
+    found at http://plone.org/documentation/manual/plone-community-developer-documentation/serving/http-request-and-response
+    @return: IP address as a string or None if not available
+    """
+    if "HTTP_X_FORWARDED_FOR" in request.environ:
+        # Virtual host
+        ip = request.environ["HTTP_X_FORWARDED_FOR"]
+    elif "HTTP_HOST" in request.environ:
+        # Non-virtualhost
+        ip = request.environ["REMOTE_ADDR"]
+    else:
+        # Unit test code?
+        ip = None
+
+    return ip
+
+IP = get_ip(request) 
+fedora = getToolByName(self,'fedora')
+fedoraIP = fedora.address
+
+
+
+if IP == fedoraIP:
 
     MIMEType = self.content_type
     type = MIMEType.split('/')[0]
@@ -33,6 +56,6 @@ if IP in ALLOWED_IPS:
         print "Unsupported MIME Type"
         return printed
 else:
-    print "Your IP (%s) is not allowed to access this content!" % (IP)
+    print "Your IP %s is not allowed to access this content!" % (IP)
     return printed
-"""
+
