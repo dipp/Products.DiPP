53f9ee64369d5a52d3244f12a83c9c70f3609423
Peter Reimer <reimer@hbz-nrw.de>
2010-09-28 12:18:01 +0000

    reommended citation moved to bibtool for easy inclusion of pagenumbers

--- BibTool.py
+++ BibTool.py
@@ -39,7 +39,7 @@ class BibTool(UniqueObject, SimpleItem):
             ("Endnote","end"),
             ("Bibtex","bib"),
             ("Reference Manger","ris"),
-            ("MESH","xml")
+            ("Mods","xml")
         )
         return formats
    
@@ -59,7 +59,88 @@ class BibTool(UniqueObject, SimpleItem):
         else:
             if level and (not elem.tail or not elem.tail.strip()):
                 elem.tail = i
-   
+    
+    def recommended_citation(self,PID,qdc):
+
+        # citation_format = self.citation_format|here/portal_properties/dipp_properties/citation_format|nothing;
+        dp = self.portal_properties.dipp_properties
+        citation_format = dp.citation_format
+        initials_only = dp.initials_only
+        comma_separated = dp.comma_separated
+        initials_period = dp.initials_period
+
+        if PID.split(':')[0] != 'temp':
+        
+            # Metadata stored only in Plone
+            try:
+                startpage = self.startpage
+            except:
+                startpage = None
+                
+            try:
+                endpage = self.endpage
+            except:
+                endpage = None
+            
+            # Qualified Dublin Core Metadata (qdc) stored in Fedora
+            authors = qdc['creatorPerson']
+            titles = qdc['title']
+            title = titles[0]['value']
+            bibliographicCitation = qdc['bibliographicCitation'][0]
+            journal = bibliographicCitation['journalTitle']
+            volume = bibliographicCitation['journalVolume']
+            issue = bibliographicCitation['journalIssueNumber']
+            issuedate = DateTime(bibliographicCitation['journalIssueDate'])
+            date = issuedate.strftime(self.portal_properties.site_properties.localTimeFormat)
+            year = issuedate.strftime('%Y')
+            urn = qdc['identifierURN'];
+            id = self.PID.split(':')[-1]
+            authors_list = ""
+            period = ""
+            comma = ""
+            
+            if initials_period:
+                period = "."
+
+            if comma_separated:
+                comma = ','
+
+            for author in authors:
+                author_number = authors.index(author) + 1
+                firstnames = author['firstName'].split()
+                firstnames_initials = ""
+                for firstname in firstnames:
+                    firstnames_initials += firstname[0] + period
+                lastname = author['lastName']
+                if author_number == len(authors):
+                    glue = ""
+                else:
+                    glue = ", "
+                if initials_only:
+                    firstname = firstnames_initials
+                authors_list += "%s%s %s%s" % (lastname, comma, firstname, glue)
+
+
+            cite = citation_format % {
+            'authors':authors_list, 
+            'title':title,
+            'journal':journal,
+            'volume':volume,
+            'issue':issue,
+            'startpage':startpage,
+            'endpage':endpage,
+            'year':year,
+            'date':date,
+            'id':id,
+            'urn': '<a href="http://nbn-resolving.de/%(urn)s">%(urn)s</a>' % {'urn':urn}
+            }
+            
+        else:
+            cite = "This is just a temporary conversion, no metadata available for PID %s. to build a citation" % PID
+            
+        return cite
+        return "formatted"
+    
     def _make_mods(self,qdc,PID):
         """Create a mods xml string for use with bibutils"""
         
