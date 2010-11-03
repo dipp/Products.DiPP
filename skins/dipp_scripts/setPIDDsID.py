fbf808aaf58262a1c3d78212c087b16719cdab1d
Peter Reimer <reimer@hbz-nrw.de>
2010-11-03 23:21:47 +0000

    helferscript um eine PID zu setzen

---
+++ skins/dipp_scripts/setPIDDsID.py
@@ -0,0 +1,31 @@
+## Script (Python) "setPIDDsID"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+
+# -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+request  = container.REQUEST
+RESPONSE = request.RESPONSE
+
+# set creationDate and effectiveDate to a given date.
+# by jensens
+
+PID = traverse_subpath[0]
+#DsID = traverse_subpath[1]
+
+if len(PID.split(':')) < 2:
+    return "PID widthout colon"
+
+# return printed
+
+context.setPID(PID)
+
+context.reindexObject()
+return "PID on '%s' successfully set to %s." % (context.title_or_id(), PID)
+
