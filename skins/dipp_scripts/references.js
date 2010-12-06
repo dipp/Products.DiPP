f21ddd61e9da5740992f036db3e59befcc92264c
Peter Reimer <reimer@hbz-nrw.de>
2010-12-06 14:57:47 +0000

    kommentare, semikolons, umsortieren...

--- skins/dipp_scripts/references.js
+++ skins/dipp_scripts/references.js
@@ -1,46 +1,57 @@
-$(document).ready(inlineFootnotes);
+/*
+ * Display the full references of a DocBook converted article
+ * as tooltips of the reference to the footnote
+ * Requires jQuery http://jquery.com/ and jQuery Tools Tooltip
+ * http://flowplayer.org/tools/tooltip/index.html   
+ *   
+ * $Id$
+ */
 
-function inlineFootnotes() {
-	var ftn = "ftn."
-	var refs = getReferences();
-	var ids = Array()
-	var content = $("#region-content");
-	var footnoteReferences = $("span.footnoteReference",content);
-	for (var i = 0; i < footnoteReferences.length; i++ ){
-		var link = $("a", footnoteReferences[i])  
-		ref_id = link.attr('id');
-		if (ref_id.substring(0,4) != ftn) {
-			id = ftn + ref_id;
-			link.after('<div class="inlineReference"></div>');
-			var tt = $("div",footnoteReferences[i])
-			tt.html(refs[id]);
-			$("span",tt).remove(".footnoteReference");
-			link.tooltip({
-						//effect: 'fade',
-						//fadeOutSpeed: 50,
-						//predelay: 400,
-						delay: 500,
-						position: "bottom right",
-						offset: [-50, -80]
-					}		
-			
-			);
-		}
-	}
-	//alert(refs[ids[2]]);
-}
 
+/* return an assocated array with the ID of the reference as key and 
+ * the full reference as value
+ */
 function getReferences() {
-	var references = Array();
+	var references = [];
 	var content = $("#region-content");  
 	var footnotes = $("div.footnotes div.footnote", content);
 	for (var i = 0; i < footnotes.length; i++ ){
-		ref_id = $("span.footnoteReference a", footnotes[i]).attr('id');
-		ref_content = $("p", footnotes[i]).html();
+		var ref_id = $("span.footnoteReference a", footnotes[i]).attr('id');
+		var ref_content = $("p", footnotes[i]).html();
 		references[ref_id] = ref_content;
-		};
-	//alert(references['ftn.d53e195']);
-	return references
-	};
+		}
+	return references;
+	}
 
-	
+
+/*
+ * scan the article for footnote references and put a div containing
+ * the full reference after the hyperlink. This div is used as tooltip 
+ */
+function inlineFootnotes() {
+	var ftn = "ftn.";   // ID prefix of the references in the footnotes
+	var refs = getReferences();
+	var content = $("#region-content");
+	var footnoteReferences = $("span.footnoteReference",content);
+	for (var i = 0; i < footnoteReferences.length; i++ ){
+		var link = $("a", footnoteReferences[i]);
+		var ref_id = link.attr('id');
+		if (ref_id.substring(0,4) != ftn) {
+			var id = ftn + ref_id;
+			link.after('<div class="inlineReference"></div>');
+			var tt = $("div",footnoteReferences[i]);
+			tt.html(refs[id]);
+			$("span",tt).remove(".footnoteReference");
+			link.tooltip({
+				//effect: 'fade',
+				//fadeOutSpeed: 50,
+				//predelay: 400,
+				delay: 500,
+				position: "bottom right",
+				offset: [-50, -80]
+			});
+		}
+	}
+}
+
+$(document).ready(inlineFootnotes);
