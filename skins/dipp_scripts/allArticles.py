ed1df6279480fbac1971d732b6513b07d0af9eac
Peter Reimer <reimer@hbz-nrw.de>
2011-02-02 22:09:18 +0000

    check nach Dubletten

--- skins/dipp_scripts/allArticles.py
+++ skins/dipp_scripts/allArticles.py
@@ -16,8 +16,43 @@ response = request.RESPONSE
 portal_url = getToolByName(context, 'portal_url')
 portal     = portal_url.getPortalObject()
 articles   = container.portal_catalog(portal_type='FedoraArticle', sort_on='getPID')
+# articles   = container.portal_catalog(portal_type='FedoraArticle')
 
+
+PIDs = []
 for article in articles:
-    print "%s, %s, %s" % (article.getPID, article.review_state, article.Title)
+    try:
+        PID = article.getObject().PID
+    except:
+        PID = "dipp:XXXX"
+    PIDs.append((PID, article.review_state, article.getURL(), article.Title))
 
+
+uniquePIDs = {}
+multiPIDs = {}
+
+urls = {}
+for PID, state, url, title in PIDs:
+    if not PID in uniquePIDs:
+        uniquePIDs[PID] = url
+    elif PID in multiPIDs:
+        multiPIDs[PID].append(url)
+    else:
+        multiPIDs[PID] = [url]
+
+for PID in multiPIDs.keys():
+    multiPIDs[PID].append(uniquePIDs[PID])
+
+
+print "PIDs", len(PIDs)
+print "unique PIDs:", len(uniquePIDs)
+print "doubles"
+for PID in multiPIDs.keys():
+    print PID
+    for url in multiPIDs[PID]:
+        print "    " + url
+print "all articles"
+for PID, state, url, title in PIDs:
+    print PID, state, title, url
 return printed
+
