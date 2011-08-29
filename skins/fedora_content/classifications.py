05ca9390464fc1fc7269b3df2341640072abc993
Peter Reimer <reimer@hbz-nrw.de>
2011-08-29 11:54:11 +0000

    jel ergänzt

--- skins/fedora_content/classifications.py
+++ skins/fedora_content/classifications.py
@@ -18,6 +18,7 @@ classifactions = [
     {'id':'rvk','title':"Regensburger Verbundklassifikation",'url':'http://www.bibliothek.uni-regensburg.de/rvko_neu/'},
     {'id':'bk', 'title':"Basis-Klassifikation", 'url':'http://www.gbv.de/vgm/info/mitglieder/02Verbund/01Erschliessung/02Richtlinien/05Basisklassifikation/index'},
     {'id':'pacs', 'title':"Physics and Astronomy Classification Scheme", 'url':'http://publish.aps.org/PACS/'},
-    {'id':'msc','title':"2000 Mathematics Subject Classification", 'url':'http://www.ams.org/msc/'}
+    {'id':'msc','title':"2000 Mathematics Subject Classification", 'url':'http://www.ams.org/msc/'},
+    {'id':'jel','title':"Journal of Economic Literature", 'url':'http://www.aeaweb.org/jel/jel_class_system.php'}
 ]
 return classifactions
