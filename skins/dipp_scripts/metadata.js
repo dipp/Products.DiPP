c4ec14dbe4a9755dc1720e90ec9e70c8263b0915
Peter Reimer <reimer@hbz-nrw.de>
2011-10-19 13:38:26 +0000

    funktioniert beinahe... wird aber erstmal deaktiviert

--- skins/dipp_scripts/metadata.js
+++ skins/dipp_scripts/metadata.js
@@ -9,9 +9,12 @@ function add_field () {
     var metadata_field = $('#creatorPerson');
     var button = $('input.addfield', metadata_field);
     var tabelle = $('table', metadata_field);
-    var last_row = $('tr', tabelle);
+    var last_row = $('tr:last', tabelle);
+    //$('input', empty_row).attr('value','');
     button.click(function() {
-        alert(last_row);
+        last_row.after("<tr class='new'>" + last_row.html() + "</tr>");
+        var new_row = $('table tr:last', metadata_field);
+        $('input', new_row).attr('value','');
         return false;
     })
 }
