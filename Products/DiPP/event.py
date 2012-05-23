131e29f45796ef710f0f9b7bc17e1fb0a4def884
Peter Reimer <reimer@hbz-nrw.de>
2012-05-23 14:22:42 +0000

    wrapper for event classes

--- Products/DiPP/event.py
+++ Products/DiPP/event.py
@@ -1,6 +1,11 @@
 from Products.DiPP import HAS_PLONE30
 
+# the event classes are kept in different modules for different Plone versions
+# this is just a wrapper which allows the use the same subscriber for Plone 2.5.5 and 4.x
+# not tested with Plone 3  
+
 if HAS_PLONE30:
-    from zope.lifecycleevent.interfaces import IObjectModifiedEvent
+    #from zope.lifecycleevent.interfaces import IObjectModifiedEvent
+    from Products.Archetypes.interfaces.event import IObjectModifiedEvent
 else:
     from zope.app.event.interfaces import IObjectModifiedEvent
