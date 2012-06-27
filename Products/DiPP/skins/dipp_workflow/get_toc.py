3dec3f84ec6eaa48bce501f9e0e82a1de0e4826e
Peter Reimer <reimer@hbz-nrw.de>
2012-06-27 16:18:37 +0000

    use boolean opertator insteat string when checking for toc_html

--- Products/DiPP/skins/dipp_workflow/get_toc.py
+++ Products/DiPP/skins/dipp_workflow/get_toc.py
@@ -1,4 +1,4 @@
-## Script (Python) "get_status"
+## Script (Python) "get_toc"
 ##bind container=container
 ##bind context=context
 ##bind namespace=
@@ -10,9 +10,12 @@
 request = container.REQUEST
 RESPONSE =  request.RESPONSE
 
+# if an article contains a Table of content in toc_html
+# return its PID/DsID, otherwise False
+
 try:
     toc = getattr(context,'toc_html')
     ids = {'PID':toc.PID,'DsID':toc.DsID}
     return ids
 except:
-    return "no toc"
+    return False
