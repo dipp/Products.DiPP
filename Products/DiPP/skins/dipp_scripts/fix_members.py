51f4ea8417a9a8113edec5606057bab8d3c2158b
Peter Reimer <reimer@hbz-nrw.de>
2013-05-15 19:59:04 +0000

    keep only members, who are also in the author index or have more than the default roles

--- Products/DiPP/skins/dipp_scripts/fix_members.py
+++ Products/DiPP/skins/dipp_scripts/fix_members.py
@@ -12,13 +12,37 @@
 from Products.CMFCore.utils import getToolByName
 request  = container.REQUEST
 RESPONSE = request.RESPONSE
-mtool = getToolByName(self, 'portal_membership')
 
+mtool = getToolByName(self, 'portal_membership')
+catalog = getToolByName(self, 'portal_catalog')
+
+authors = catalog.uniqueValuesFor('getAuthors')
+fullnames = []
+
+for author in authors:
+    try:
+        parts = author.split(',')
+        fullnames.append(parts[1].strip() + ' ' + parts[0].strip())
+    except:
+        print "invalid name: %s" % author
+fullnames = tuple(fullnames)
+
+keep_members = []
 members = mtool.listMembers()
 for member in members:
-    print member.id, member.getRoles()
+    keep = " "
+    user_id = member.id
+    memberinfo = mtool.getMemberInfo(user_id)
+    fullname = memberinfo['fullname']
+    roles = filter(lambda role: role not in ('Member','Authenticated'), member.getRoles())
+    
+    if fullname in fullnames or len(roles) > 0:
+        keep = "*"
+        keep_members.append(user_id)
+
+    print keep, member.id, member.getRoles(), roles,  fullname 
 
 print "\nNumber of Members: %s" % len(members)
-
+print "Keep: ", keep_members
 return printed
 
