719305715cc71739cc7f9541b368d24a35d484b0
Peter Reimer <reimer@hbz-nrw.de>
2013-11-14 14:10:09 +0000

    doaj xml downloadable

--- Products/DiPP/skins/dipp_scripts/doaj.py
+++ Products/DiPP/skins/dipp_scripts/doaj.py
@@ -20,21 +20,22 @@ catalog = getToolByName(self, 'portal_catalog')
 bibtool = getToolByName(self, 'bibtool')
 
 results = catalog(portal_type=('FedoraArticle'), Language='all', path=path,sort_on='getPID', sort_order='reverse')
+
 pids = []
+
 for result in results:
     pids.append(result.getPID)
 
-skipped, xml = bibtool.doaj_xml(pids)
-
+(skipped, xml) = bibtool.doaj_xml(pids)
+self.plone_log("Skipped PIDs" + str(skipped))
 if len(sub_path) == 0:
     if len(skipped) > 0:
-        portal_status_message = translate('field-removed', domain='qdc')
-        portal_status_message = "skipped" + str(skipped)
+        portal_status_message = "skipped: " + str(skipped)
         context.plone_utils.addPortalMessage(portal_status_message)
     return xml
 
 elif len(sub_path) == 1 and sub_path[0] == 'download':
-        print xml
-        RESPONSE.headers['Content-disposition'] = 'attachment; filename=doaj.xml'
-        RESPONSE.headers['Content-type'] = 'text/xml; charset="utf-8'
-        return printed
+    print xml
+    RESPONSE.headers['Content-disposition'] = 'attachment; filename=doaj.xml'
+    RESPONSE.headers['Content-type'] = 'text/xml; charset="utf-8'
+    return printed
