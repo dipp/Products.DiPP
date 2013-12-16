d0e032b8b9ea877d47bdab601d27474a467a2cbc
Peter Reimer <reimer@hbz-nrw.de>
2013-12-16 20:35:55 +0000

    Ticket DIP-66 ein klein wenig Doku für ein seeehr rudimentäres Importscript

--- Products/DiPP/Extensions/fedora_import.py
+++ Products/DiPP/Extensions/fedora_import.py
@@ -1,27 +1,47 @@
-from fedora2.FedoraAccess import *
-from fedora2.FedoraManagement import *
+from dipp.fedora2.FedoraAccess import *
+from dipp.fedora2 import FedoraManagement
+from StringIO import StringIO
 import os
 
-management = FedoraManagement()
+fedoramanagement = FedoraManagement.FedoraManagement()
 
+
+# Batchimport eines Verzeichnisse mit Audiodateien nach Fedora und Plone
+#
+# im Artikelordner musse eine externe Methode wie folgt angelegt werden:
+#
+# Id: fedora_import (beliebig)
+# Title: import from filesystem (beliebig)
+# Module Name: DiPP.fedora_import
+# Function Name: fedora_import
+
+# Verzeichnis mit den mp3 Dateien 
+FILESYSTEM_DIR = "/files/var/www/import/"
+
+# URL, unter der das Verzeichnis abbrufbar ist
+WEB_DIR = "http://alkyoneus.hbz-nrw.de/import/"
+
+# PID des Fedoraobjekts, in das die Dateien als Datenströme 
+# abgelegt werden sollen
+PID = "content:21766"
 
 def fedora_import(self):
-    WEB_DIR = "http://www.afrikanistik-online.de/download/"
-    FILESYSTEM_DIR = "/themisfiles/var/www/afrika/download/"
-    PID = "content:13451"
+    out = StringIO()
     MIMEType = "audio/mpeg"
     ControlGroup = "M"
     MDClass = ""
     MDType = ""
     DsState = "A"
     dirList = os.listdir(FILESYSTEM_DIR)
-
     for file_name in dirList:
-        #if file_name == "WoohAli.mp3":
         file = open(FILESYSTEM_DIR + file_name)
         Label = file_name
         Location = WEB_DIR + file_name
-        DsID = management.addDatastream(PID,Label,MIMEType,Location,ControlGroup,MDClass,MDType,DsState)._response
+        DsID = fedoramanagement.addDatastream(PID,Label,MIMEType,Location,ControlGroup,MDClass,MDType,DsState)
         self.invokeFactory('FedoraMultimedia', id=file_name,File=file, title=file_name, PID=PID, DsID=DsID)
-        print DsID, file_name
-
+        audio = getattr(self, file_name)
+        audio.manage_addProperty(id='layout', value="mmmp3_view", type='string')
+        audio.reindexObject()
+        print >> out,  Location, DsID
+    
+    return out.getvalue()
