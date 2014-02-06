10ce0cc0bc3701c3cfb1b178ffe6cd7e0ecf075b
Peter Reimer <reimer@hbz-nrw.de>
2014-02-06 09:16:26 +0000

    doaj and datacite xml export merged on metadata_export form

--- Products/DiPP/skins/dipp_scripts/doaj.py
+++ Products/DiPP/skins/dipp_scripts/doaj.py
@@ -14,28 +14,20 @@ sub_path = traverse_subpath
 translate = context.translate
 
 from Products.CMFCore.utils import getToolByName
-
-path = '/'.join(self.getPhysicalPath())
-catalog = getToolByName(self, 'portal_catalog')
 bibtool = getToolByName(self, 'bibtool')
 
-results = catalog(portal_type=('FedoraArticle'), Language='all', path=path,sort_on='getPID', sort_order='reverse')
+pid = self.PID
+doi = self.DOI
+main_issn = self.portal_properties.metadata_properties.issn
 
-pids = []
+try:
+    issn = self.issn
+except:
+    issn =  main_issn
 
-for result in results:
-    pids.append(result.getPID)
-
-(skipped, xml) = bibtool.doaj_xml(pids)
-self.plone_log("Skipped PIDs" + str(skipped))
-if len(sub_path) == 0:
-    if len(skipped) > 0:
-        portal_status_message = "skipped: " + str(skipped)
-        context.plone_utils.addPortalMessage(portal_status_message)
-    return xml
-
-elif len(sub_path) == 1 and sub_path[0] == 'download':
-    print xml
-    RESPONSE.headers['Content-disposition'] = 'attachment; filename=doaj.xml'
-    RESPONSE.headers['Content-type'] = 'text/xml; charset="utf-8'
-    return printed
+publisher = self.portal_properties.metadata_properties.publisher
+pdf = self.getFulltextPdf()
+xml = bibtool.doaj_xml(pid,issn=issn,publisher=publisher,pdf=pdf)
+RESPONSE.headers['Content-disposition'] = 'attachment; filename=%s.xml' % pid.replace(':','_')
+RESPONSE.headers['Content-type'] = 'text/xml; charset="utf-8'
+return xml
