f47cc8833f7942efd7bb5da267de5add62c04b43
Peter Reimer <reimer@hbz-nrw.de>
2014-07-07 16:12:28 +0000

    add additional author fields via javasript

--- Products/DiPP/skins/dipp_scripts/metadata.js
+++ Products/DiPP/skins/dipp_scripts/metadata.js
@@ -4,20 +4,43 @@
  * $Id$
  */
 
+function get_next_ids(current_id) {
+    var sep = '_'
+    var parts = current_id.split(sep);
+    var number = parseInt(parts[1], 10) + 1;
+    return [number, parts[0] + sep + number];
+}
+
 
 function add_field () {
     var metadata_field = $('#creatorPerson');
     var button = $('input.addfield', metadata_field);
-    var tabelle = $('table', metadata_field);
-    var last_row = $('tr:last', tabelle);
-    //$('input', empty_row).attr('value','');
-    button.click(function() {
-        last_row.after("<tr class='new'>" + last_row.html() + "</tr>");
-        var new_row = $('table tr:last', metadata_field);
+    //$("form").submit(function (event) {
+    button.click(function (event) {
+        event.preventDefault();
+        var tabelle = $('table', metadata_field);
+        var last_row = $('tr:last', tabelle);
+        var last_id = last_row.attr('id');
+        var next_ids = get_next_ids(last_id);
+        console.log(next_ids);
+        var new_row = $('tr:last', tabelle).clone().appendTo(tabelle);
         $('input', new_row).attr('value','');
+        $('.counter', new_row).attr('value',next_ids[0]);
+
+        if (new_row.hasClass('odd')) {
+            new_row.removeClass('odd');
+            new_row.addClass('even');
+        } else {
+            new_row.removeClass('even');
+            new_row.addClass('odd');
+        };
+        new_row.attr('id', next_ids[1])
+        
+        
         return false;
     })
 }
+
 
 (function($) {
 	$(document).ready(add_field);
