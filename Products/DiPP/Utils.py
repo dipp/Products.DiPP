aeafae7c6076ffe2b87e448a9526211b3a69daa7
Peter Reimer <reimer@hbz-nrw.de>
2017-09-22 15:28:43 +0200

    pep8 stuff

--- Products/DiPP/Utils.py
+++ Products/DiPP/Utils.py
@@ -20,18 +20,20 @@ import Permissions
 
 logger = logging.getLogger(__name__)
 
-#VERIFY_SERVER="www.google.com"
+# VERIFY_SERVER="www.google.com"
+
 
 class Utils(UniqueObject, SimpleItem):
-    """ Utilities for e.g. reCAPTCHA """
-    
+    """Utilities for e.g. reCAPTCHA."""
+
     meta_type = 'Utils'
     id = 'Utils'
     title = 'Utilities'
     security = ClassSecurityInfo()
 
-    def get_ip(self,request):
-        """  Extract the client IP address from the HTTP request in proxy compatible way.
+    def get_ip(self, request):
+        """Extract the client IP address from the HTTP request in proxy compatible way.
+
         found at http://plone.org/documentation/manual/plone-community-developer-documentation/serving/http-request-and-response
         @return: IP address as a string or None if not available
         """
@@ -48,13 +50,15 @@ class Utils(UniqueObject, SimpleItem):
         return ip
 
     def get_location(self, path, request):
-        """return the URL of an uploaded file based on ip and port, not rewritten
-        by VHM/Apache and without https. Requires a FQDN of the server running zope"""
-        
+        """Return the URL of an uploaded file based on ip and port.
+
+        not rewritten by VHM/Apache and without https. Requires a
+        FQDN of the server running zope
+        """
         address = socket.gethostbyname(socket.getfqdn())
         port = request.environ["SERVER_PORT"]
         netloc = ':'.join((address, port))
-        url = urlparse.urlunparse(('http',netloc,'/'.join(path),'','',''))
+        url = urlparse.urlunparse(('http', netloc, '/'.join(path), '', '', ''))
         logger.info(url)
         return url
 
@@ -64,37 +68,38 @@ class Utils(UniqueObject, SimpleItem):
         return s
 
     def get_recaptcha_keys(self):
-        """Return the reCAPTCHA keys from the config file for use in pagetemplates """
-        return {'private':PRIVATE_KEY, 'public': PUBLIC_KEY}
+        """Return the reCAPTCHA keys from the config file for use in pagetemplates."""
+        return {'private': PRIVATE_KEY, 'public': PUBLIC_KEY}
 
     def validate_recaptcha(self, recaptcha_challenge_field, recaptcha_response_field, remoteip):
-        """ Captcha code from
+        """Validate recaptcha.
+
+        Code from
         http://recaptcha.googlecode.com/svn/trunk/recaptcha-plugins/python/recaptcha/client/captcha.py
         """
+        params = urllib.urlencode({
+            'privatekey': self.encode_if_necessary(PRIVATE_KEY),
+            'remoteip': self.encode_if_necessary(remoteip),
+            'challenge': self.encode_if_necessary(recaptcha_challenge_field),
+            'response': self.encode_if_necessary(recaptcha_response_field),
+        })
 
-        params = urllib.urlencode ({
-                'privatekey': self.encode_if_necessary(PRIVATE_KEY),
-                'remoteip' :  self.encode_if_necessary(remoteip),
-                'challenge':  self.encode_if_necessary(recaptcha_challenge_field),
-                'response' :  self.encode_if_necessary(recaptcha_response_field),
-                })
-
-        request = urllib2.Request (
-            url = "https://%s/recaptcha/api/verify" % VERIFY_SERVER,
-            data = params,
-            headers = {
+        request = urllib2.Request(
+            url="https://%s/recaptcha/api/verify" % VERIFY_SERVER,
+            data=params,
+            headers={
                 "Content-type": "application/x-www-form-urlencoded",
                 "User-agent": "reCAPTCHA Python"
-                }
-            )
-        
+            }
+        )
+
         httpresp = urllib2.urlopen(request)
 
-        return_values = httpresp.read().splitlines();
-        httpresp.close();
+        return_values = httpresp.read().splitlines()
+        httpresp.close()
 
         return_code = return_values[0]
-        
+
         if return_code == "true":
             is_valid = True
             error_code = None
@@ -102,7 +107,7 @@ class Utils(UniqueObject, SimpleItem):
             is_valid = False
             error_code = return_values[1]
 
-        logger.info("reCAPTCHA - IP: %s, response: %s, error code: %s" % (remoteip, recaptcha_response_field, error_code))        
+        logger.info("reCAPTCHA - IP: %s, response: %s, error code: %s" % (remoteip, recaptcha_response_field, error_code))
         return is_valid, error_code
 
     """
@@ -123,4 +128,3 @@ class Utils(UniqueObject, SimpleItem):
         urls = [urlparse.urlparse(url)[2] for url in urls]
         return stats.get_data(year, urls)
     """
-    
