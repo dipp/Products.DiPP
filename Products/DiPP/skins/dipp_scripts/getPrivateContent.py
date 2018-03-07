835c4d45be79da19da8979aa3e6e2d78e5d3c4fe
Peter Reimer <reimer@hbz-nrw.de>
2018-03-07 09:56:02 +0100

    lots of PEP8 stuff, no actual code changes

--- Products/DiPP/skins/dipp_scripts/getPrivateContent.py
+++ Products/DiPP/skins/dipp_scripts/getPrivateContent.py
@@ -10,20 +10,18 @@
 
 # -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
-request  = container.REQUEST
+request = container.REQUEST
 RESPONSE = request.RESPONSE
-
-
 
 """Allow fedora to fetch private content without authentication. This script runs with proxyrole Manager
    but can only be called from the machine where fedora is installed.
 """
 
 
-
 def get_ip(request):
-    """  Extract the client IP address from the HTTP request in proxy compatible way.
-    found at http://plone.org/documentation/manual/plone-community-developer-documentation/serving/http-request-and-response
+    """ Extract the client IP address from the HTTP request in proxy compatible way.
+
+    Found at http://plone.org/documentation/manual/plone-community-developer-documentation/serving/http-request-and-response
     @return: IP address as a string or None if not available
     """
     if "HTTP_X_FORWARDED_FOR" in request.environ:
@@ -38,18 +36,19 @@ def get_ip(request):
 
     return ip
 
-IP = get_ip(request) 
-fedora = getToolByName(self,'fedora')
+
+IP = get_ip(request)
+fedora = getToolByName(self, 'fedora')
 
 FEDORA_IP, FEDORA_PORT = fedora.getServerConfiguration()
 context.plone_log((FEDORA_IP, FEDORA_PORT))
 
 if IP == FEDORA_IP:
 
-    context.plone_log ("getPrivateContent: trying to fetch  '%s' (%s)" % (self.id, IP))
+    context.plone_log("getPrivateContent: trying to fetch  '%s' (%s)" % (self.id, IP))
     MIMEType = self.content_type
-    MIMETypeCategory = MIMEType.split('/')[0] 
-    
+    MIMETypeCategory = MIMEType.split('/')[0]
+
     RESPONSE.setHeader('Content-Type', MIMEType)
     if MIMETypeCategory in ['application', 'image', 'audio', 'video']:
         cd = 'attachment; filename=%s' % (id)
@@ -59,11 +58,10 @@ if IP == FEDORA_IP:
     if MIMETypeCategory == 'text':
         return self.body
     else:
-        print "Unsupported MIME Type"
+        print "Unsupported MIME Typ"
         return printed
 else:
     msg = "Your IP %s is not allowed to access this content!" % (IP)
     print msg
-    context.plone_log (msg)
+    context.plone_log(msg)
     return printed
-
