a5301f66ff81a91969f03cace63ee9a63c3e0958
Peter Reimer <reimer@hbz-nrw.de>
2018-03-14 18:34:22 +0100

    some indendation and whitespace issues

--- Products/DiPP/content/fedoramultimedia.py
+++ Products/DiPP/content/fedoramultimedia.py
@@ -65,47 +65,46 @@ FedoraMultimediaSchema = BaseSchema + Schema((
                 vocabulary = "getTypeOfList"
         )
     ),
-    marshall = PrimaryFieldMarshaller()
+    marshall=PrimaryFieldMarshaller()
 )
+
 
 class FedoraMultimedia(BrowserDefaultMixin, BaseContent):
     """Multimedia files (Images, PDF, Movies) for storing in Fedora"""
-    
-    security = ClassSecurityInfo()
-    __implements__ = (getattr(BrowserDefaultMixin,'__implements__',()),) + (getattr(BaseContent,'__implements__',()),)
-    implements(IIndexableContent, IFedoraMultimedia)
-    
-    schema = FedoraMultimediaSchema
-    
-    inlineMimetypes= ('application/msword',
-                      'application/vnd.ms-excel',
-                      'application/vnd.ms-powerpoint',
-                      'application/rtf',
-                      'application/pdf')
 
+    security = ClassSecurityInfo()
+    __implements__ = (getattr(BrowserDefaultMixin, '__implements__', ()),) + (getattr(BaseContent, '__implements__ ', ()),)
+    implements(IIndexableContent, IFedoraMultimedia)
+
+    schema = FedoraMultimediaSchema
+
+    inlineMimetypes = ('application/msword',
+                       'application/vnd.ms-excel',
+                       'application/vnd.ms-powerpoint',
+                       'application/rtf',
+                       'application/pdf')
 
     security = ClassSecurityInfo()
 
     def reindex_article(self):
-        """ if this object is a pdf version of the article the article has to be 
+        """ if this object is a pdf version of the article the article has to be
             reindexed upon changes/creation.
         """
         if self.MMType == "alternative_format":
             article = self.getParentNode()
             article.reindexObject()
             logger.info("reindexed %s" % article.PID)
-    
 
     def at_post_create_script(self):
         """ reindex article folder when pdf flltext is added
         """
         self.reindex_article()
-    
+
     def at_post_edit_script(self):
         """ reindex article folder when pdf flltext is modified
         """
         self.reindex_article()
-    
+
     def indexableContent(self, fields):
         """get the binary datastream from fedora and return in to TextIndexNG
            Only pdf Files should be indexed (via pdftotext) mimetype is checked in plone
@@ -116,29 +115,29 @@ class FedoraMultimedia(BrowserDefaultMixin, BaseContent):
         PID = self.PID
         DsID = self.DsID
         MIMEType = self.get_content_type()
-        if PID and DsID and MIMEType == "application/pdf": 
-            data =  fedora.accessMultiMediaByFedoraURL(PID,DsID,None)
+        if PID and DsID and MIMEType == "application/pdf":
+            data = fedora.accessMultiMediaByFedoraURL(PID, DsID, None)
             stream = data['stream']
             logger.info("Fetching %s/%s for indexing." % (PID, DsID))
-            icc.addBinary('SearchableText', 
+            icc.addBinary('SearchableText',
                           stream,
                           MIMEType,
                           'iso-8859-15',
                           None)
             return icc
         return None
-        
+
     security.declareProtected(View, 'index_html')
     def index_html(self, REQUEST=None, RESPONSE=None):
         """ fetch a file from the repository
         """
-        
+
         fedora = getToolByName(self, 'fedora')
         PID = self.PID
         DsID = self.DsID
-        data =  fedora.accessMultiMediaByFedoraURL(PID,DsID,None)
+        data = fedora.accessMultiMediaByFedoraURL(PID, DsID, None)
         stream = data['stream']
-        MIMEType =data['MIMEType']
+        MIMEType = data['MIMEType']
         RESPONSE.setHeader('Content-Type', MIMEType)
         return stream
 
@@ -151,7 +150,7 @@ class FedoraMultimedia(BrowserDefaultMixin, BaseContent):
         if field.getContentType(self) in self.inlineMimetypes:
             # return the PDF and Office file formats inline
             return ATCTFileContent.index_html(self, REQUEST, RESPONSE)
-        # otherwise return the content as an attachment 
+        # otherwise return the content as an attachment
         # Please note that text/* cannot be returned inline as
         # this is a security risk (IE renders anything as HTML).
         return field.download(self)
