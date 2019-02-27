fe8f003bf172e87719c36e4d114402710eaf78e6
Peter Reimer <reimer@hbz-nrw.de>
2019-02-27 13:04:33 +0100

    bump year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Digital Peer Publishing'
-copyright = u'2012-2018, Dr. Peter Reimer, hbz'
+copyright = u'2012-2019, Dr. Peter Reimer, hbz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
