b73f12d7fd775b8fc90350f746357aedda4f68cb
Peter Reimer <reimer@hbz-nrw.de>
2019-05-17 15:27:38 +0200

    PEP8 Stuff, checking only urls of current directory

--- Products/DiPP/skins/dipp_scripts/URLcheck.py
+++ Products/DiPP/skins/dipp_scripts/URLcheck.py
@@ -10,36 +10,39 @@
 
 # -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
-request  = container.REQUEST
+request = container.REQUEST
 RESPONSE = request.RESPONSE
 
 portal_url = getToolByName(self, 'portal_url')
-fedora     = getToolByName(self, 'fedora')
-portal     = portal_url.getPortalObject()
-articles   = container.portal_catalog(portal_type='FedoraArticle', Language="all", sort_on='Date', review_state=['visible','published'])
+fedora = getToolByName(self, 'fedora')
+portal = portal_url.getPortalObject()
+folderpath = '/'.join(self.getPhysicalPath())
+# articles   = container.portal_catalog(portal_type='FedoraArticle', Language="all", sort_on='Date', review_state=['visible','published'])
 
-print '#', len(articles)
+articles = container.portal_catalog(portal_type='FedoraArticle', path=folderpath, Language="all", sort_on='Date')
+print folderpath
+print '# ' + str(len(articles))
 print 'fix:', fix
 
 for article in articles:
-    obj       = article.getObject()
-    PID       = obj.PID
-    ploneURL  = obj.absolute_url()
-    if ploneURL.startswith('https'): 
-        ploneURL = ploneURL.replace('https','http',1)
+    obj = article.getObject()
+    PID = obj.PID
+    ploneURL = obj.absolute_url()
+    #if ploneURL.startswith('https'):
+    #    ploneURL = ploneURL.replace('https','http',1)
     fedoraURL = fedora.getQualifiedDCMetadata(PID)['identifierURL']
-    
+
     if ploneURL == fedoraURL:
         OK = "OK"
     else:
         if fix == "1":
             fedora.setURL(PID, ploneURL)
-            fedora.setPublishingState(PID,1,1)
+            fedora.setPublishingState(PID, 1, 1)
             OK = "FIXED"
         else:
             OK = "BROKEN"
-    
+
     print PID, OK
-    print "\t p",ploneURL
-    print "\t f",fedoraURL
+    print "\t p " + str(ploneURL)
+    print "\t f " + str(fedoraURL)
 return printed
